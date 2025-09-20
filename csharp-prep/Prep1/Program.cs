cf90138eb74d7950e04743d2bcce100bc65cdd4c
brycekt887 <brycekt@byui.edu>
2025-09-20 12:24:16 -0600

    Finshed prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name1 = Console.ReadLine();
+        Console.Write("What is your last name?  ");
+        string name2 = Console.ReadLine();
+        Console.WriteLine($"Your name is {name2}, {name1}  {name2}");
     }
 }
