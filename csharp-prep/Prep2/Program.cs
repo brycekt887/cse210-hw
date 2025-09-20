9837efa5107994f5585517f530ebc8aa147ce3e6
brycekt887 <brycekt@byui.edu>
2025-09-20 12:35:09 -0600

    Prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string Grader = "";
+
+        if (percent >= 90)
+        {
+            Grader = "A";
+        }
+        else if (percent >= 80)
+        {
+            Grader = "B";
+        }
+        else if (percent >= 70)
+        {
+            Grader = "C";
+        }
+        else if (percent >= 60)
+        {
+            Grader = "D";
+        }
+        else
+        {
+           Grader = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {Grader}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("O no! Better Luck Next Time");
+        }
     }
 }
