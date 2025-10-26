48e56cb39359edf9edabb4da678f9934974c10aa
brycekt887 <brycekt@byui.edu>
2025-10-25 22:16:16 -0600

    moved to correct file location

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        // Split the text into words
+        string[] parts = text.Split(" ");
+        foreach (string word in parts)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = _random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + "\n";
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText.Trim();
+    }
+}
