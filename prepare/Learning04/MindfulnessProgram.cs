44bbc12bba115a20482a897616a8195eb70beb3b
brycekt887 <brycekt@byui.edu>
2025-11-08 15:38:29 -0700

    first stretch

---
+++ prepare/Learning04/MindfulnessProgram.cs
@@ -0,0 +1,18 @@
+using System;
+
+// Main program that shows the menu and runs each activity.
+public class MindfulnessProgram
+{
+    public void DisplayMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Mindfulness Program");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflection Activity");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Quit");
+        Console.WriteLine();
+    }
+
+    
